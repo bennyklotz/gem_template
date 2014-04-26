b8230aac71cb334541e2fd2643200c53efe5459d
Benny <r3qnbenni@gmail.com>
2014-04-26 21:28:05 +0200

    wrap testclass into GemTemplate module

--- test/gem_template/zombie_test.rb
+++ test/gem_template/zombie_test.rb
@@ -1,20 +1,22 @@
 require 'test_helper'
 
-class ZombieTest < MiniTest::Unit::TestCase
-  def setup
-    # runs before each test
-    @zombie = Zombie.new
-  end
+module GemTemplate
+  class ZombieTest < MiniTest::Unit::TestCase
+    def setup
+      # runs before each test
+      @zombie = Zombie.new
+    end
 
-  def test_zombie_is_zombie
-    assert_instance_of(Zombie, @zombie, "Zombie should be zombie")
-  end
+    def test_zombie_is_zombie
+      assert_instance_of(Zombie, @zombie, "Zombie should be zombie")
+    end
 
-  def test_zombie_needs
-    assert_equal('brains', @zombie.needs, 'Zombie need brains')
-  end
+    def test_zombie_needs
+      assert_equal('brains', @zombie.needs, 'Zombie need brains')
+    end
 
-  def teardown
-    # runs after each test
+    def teardown
+      # runs after each test
+    end
   end
 end
