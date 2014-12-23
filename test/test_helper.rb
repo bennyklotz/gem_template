57fe36addd29e086c201aec70dd6bce3ff0b3be4
Benny Klotz <r3qnbenni@gmail.com>
2014-12-23 12:55:10 +0100

    use spec style formatter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,8 +23,11 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/mock'
 
-reporter_options = { color: true, slow_count: 5 }
-Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+Minitest::Reporters.use!(
+  Minitest::Reporters::SpecReporter.new,
+  ENV,
+  Minitest.backtrace_filter
+)
 
 # require 'rack/test'
 # require 'json'
